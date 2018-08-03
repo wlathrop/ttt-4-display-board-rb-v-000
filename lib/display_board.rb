c98c9442cb6ae4a41b54a6ac91e5e94439ca591a
William Lathrop <wlathrop10@gmail.com>
2018-08-03 21:39:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@ board = [" ","  ","  "," ","  ","  ","  ","  ","  "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |#{x}|   "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
