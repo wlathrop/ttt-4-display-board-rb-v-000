92830feebf6b54598e5c1e6f8d5a43e0c22d29f2
William Lathrop <wlathrop10@gmail.com>
2018-08-03 21:58:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@ def display_board(board)
   puts "-----------"
   puts "  #{board[3]} |#{board[4]}   | #{board[5]}  "
   puts "-----------"
-  puts "   |   |   "
+  puts "  #{board[6]} |   |   "
 end
 
 
